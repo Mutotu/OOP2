a324a56e3cfa7e0f4fca69df7efad50f23d02634
mutotu <muttalip.aydemir@gmail.com>
2023-01-16 15:24:16 -0800

    adding moregenreics

---
+++ src/League.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class League <T extends Team>{
+    public String name;
+    private ArrayList<T> league = new ArrayList<T>();
+
+    public League(String name) {
+        this.name = name;
+    }
+    public  boolean add(T team){
+        if(league.contains(team)) return false;
+        league.add(team);
+        return true;
+    }
+    public void showLeagueTable(){
+//        Collections.sort();
+        for(T t : league){
+            System.out.println(t.getName() + " ");
+        }
+    }
+}
--- src/Main2.java
+++ src/Main2.java
@@ -9,19 +9,31 @@ public class Main2 {
 //        items.add(4);
 //        items.add(5);
 //        printDoubled(items);
-        FootballPlayer joe = new FootballPlayer("Joe");
-        BaseBallPlayer pat = new BaseBallPlayer("Pat");
-        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+//        FootballPlayer joe = new FootballPlayer("Joe");
+//        BaseBallPlayer pat = new BaseBallPlayer("Pat");
+//        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+//
+//        Team<FootballPlayer> crows= new Team<>("Crows");
+//        crows.addPlayer(joe);
+////        crows.addPlayer(pat);
+////        crows.addPlayer(beckham);
+//        System.out.println(crows.numPlayer());
+//        Team<BaseBallPlayer> baseBallPlayerTeam = new Team<>("Chicago Cubs");
+//        baseBallPlayerTeam.addPlayer(pat);
+//        Team<SoccerPlayer> brokenTeam = new Team<>("This won't work");
+//        brokenTeam.addPlayer(beckham);
+        Gen<Integer> iOb = new Gen<Integer>(88);
 
-        Team<FootballPlayer> crows= new Team<>("Crows");
-        crows.addPlayer(joe);
-//        crows.addPlayer(pat);
-//        crows.addPlayer(beckham);
-        System.out.println(crows.numPlayer());
-        Team<BaseBallPlayer> baseBallPlayerTeam = new Team<>("Chicago Cubs");
-        baseBallPlayerTeam.addPlayer(pat);
-        Team<SoccerPlayer> brokenTeam = new Team<>("This won't work");
-        brokenTeam.addPlayer(beckham);
+        iOb.showType();
+
+        int v = iOb.getOb();
+        System.out.println("value + " + v);
+
+        Gen<String> s = new Gen<>("LOL");
+
+        System.out.println("Value : " + s.getOb() + " type is " );
+        s.showType();
+
     }
     private static void printDoubled(ArrayList<Integer> n){
 //        for(Object i: n){
